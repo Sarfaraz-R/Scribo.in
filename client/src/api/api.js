936272fa794b622590db27aaa0e010771c87dd0c
Sarfaraz <sarfrazchowdri07@gmail.com>
2026-03-27 00:55:14 +0530

    Updated backend url

--- client/src/api/api.js
+++ client/src/api/api.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 import { store } from '../store/store';
 import { setUser, logout } from '../store/slice/auth.slice';
 
-const BASE_URL = import.meta.env.VITE_API_URL || 'https://api.scribo.in';
+const BASE_URL = import.meta.env.VITE_API_URL || 'https://api.scribo.in/api';
 
 const api = axios.create({
   baseURL: BASE_URL,
